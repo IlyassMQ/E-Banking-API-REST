12f25c009c6cebb5db2e1861d122f59de0ac35d9
Ilyass Mouqerassou <ilyassmouqerassou0@gmail.com>
2026-03-27 02:02:27 +0100

    feat: add account creation validation and birth_date rule

--- app/Http/Requests/CreateAccountRequest.php
+++ app/Http/Requests/CreateAccountRequest.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace App\Http\Requests\Auth;
+namespace App\Http\Requests;
 
 use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
-class RegisterRequest extends FormRequest
+class CreateAccountRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -23,10 +23,12 @@ class RegisterRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => 'required |string',
-            'email' => 'required |email|unique:users,email',
-            'password' => 'required|string|min:8',
-            'birth_date'=>'required|date'
+        'type' => 'required|in:Checking,Saving,Minor',
+        'overdraft_limit' => 'required_if:type,Saving|nullable|numeric|min:0',
+        'interest_rate' => 'required_if:type,Minor|nullable|numeric|min:0|max:100',
+        'guardian_id' => 'required_if:type,Minor|nullable|exists:users,id',
+        'child_id' => 'required_if:type,Minor|exists:users,id'
         ];
     }
 }
+
