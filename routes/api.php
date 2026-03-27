a1ed95f6032cd73b6f13f38b550be0d2406108ca
Ilyass Mouqerassou <ilyassmouqerassou0@gmail.com>
2026-03-27 02:04:13 +0100

    feat: add API routes for accounts

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,10 @@
 <?php
 
-
+use App\Http\Controllers\AccountController;
 use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 
-
 Route::prefix('auth')->group(function () {
-
-    
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/login', [AuthController::class, 'login']);
 
@@ -16,5 +13,13 @@ Route::prefix('auth')->group(function () {
         Route::post('/logout', [AuthController::class, 'logout']);
         Route::post('/refresh', [AuthController::class, 'refresh']);
     });
+});
 
+Route::middleware('auth:api')->group(function () {
+    
+    Route::post('/accounts', [AccountController::class, 'store']);
+    Route::get('/accounts/{id}', [AccountController::class, 'show']);    
+    Route::post('/accounts/{id}/co-owner', [AccountController::class, 'addCoOwner']);    
+    Route::patch('/accounts/{id}/close', [AccountController::class, 'close']);
+    
 });
